f95ab9818f1ee8e49d66866e71b3f55542b3a2cc
Your clearpwdpwdcleargit config --global user.name Your <youremail@huskers.unl.edu>
2024-08-29 13:01:58 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Antonio Jimenez
+ * Date: 2024/08/29
+ *
+ * A simple program to print your name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Antonio Jimenez\n");
+
+  return 0;
+}
